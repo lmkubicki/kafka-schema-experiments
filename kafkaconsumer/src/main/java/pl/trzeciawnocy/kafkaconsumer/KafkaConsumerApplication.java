5a8e8e48ea562b51c3a7edddc90a950e9d472c61
Łukasz Kubicki <lmkubicki@gmail.com>
2026-04-20 20:37:11 +0200

    Small fixes

--- kafkaconsumer/src/main/java/pl/trzeciawnocy/kafkaconsumer/KafkaConsumerApplication.java
+++ kafkaconsumer/src/main/java/pl/trzeciawnocy/kafkaconsumer/KafkaConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaconsumerApplication {
+public class KafkaConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaconsumerApplication.class, args);
+        SpringApplication.run(KafkaConsumerApplication.class, args);
     }
 
 }
